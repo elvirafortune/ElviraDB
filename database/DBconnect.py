583d46c53872efa5f12edc01c9887a8a6be38032
kolesofortuni <kolesofortuni@mail.ru>
2014-04-16 19:58:34 +0400

    utf8
    utf8

--- database/DBconnect.py
+++ database/DBconnect.py
@@ -6,5 +6,6 @@ import MySQLdb.cursors
 def connect():
     db = MySQLdb.connect(host='localhost', port=3306, db='ElviraApiDb',
     user='root', passwd='',
+    charset='utf8',
     cursorclass=MySQLdb.cursors.SSDictCursor)
     return db
