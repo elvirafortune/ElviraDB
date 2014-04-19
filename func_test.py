29d0e0d9503a145580ab6f4a54896583e810f5c2
kolesofortuni <kolesofortuni@mail.ru>
2014-04-19 20:25:17 +0400

    as
    sas

--- func_test.py
+++ func_test.py
@@ -806,7 +806,7 @@ if __name__ == '__main__':
                       action="store_true", default=False)
     (options, args) = parser.parse_args()
     DEBUG = options.is_debug_mode
-    students  = {u'Иван Иванов': {'ip': u'http://193.167.2.39', 'email': u'kolesofortuni@mail.ru'}}
+    students  = {u'Иван Иванов': {'ip': u'http://193.167.2.39/db/api/', 'email': u'kolesofortuni@mail.ru'}}
     for name, info in sorted(students.items(), key=lambda t: t[0]):
         name_utf = name.encode('utf-8')
         info['ip'] = info['ip'].encode('utf-8')
